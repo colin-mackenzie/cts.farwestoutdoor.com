116c0038ab3f2d8c8312270cf4b0bdcbbc5a9ce4
Colin Mackenzie <cmackenzie@localhost.localdomain>
2015-05-25 00:14:46 -0600

    mvp

--- s/gmaps_functions_v5.js
+++ s/gmaps_functions_v5.js
@@ -1,13 +1,48 @@
 var infoWindow = null
-function savePrice(code, id){
-	if($("#"+code).val()){
-		$.post('bin/set_price.php','price='+$("#"+code).val()+'&id='+id,
-			 function(data, status, xhr){
-			   $('#error').html('Price Saved');	
-			   location.reload();
-			 });  
+
+function RemoveBrick(obj){
+	$(obj).closest('tr').remove();
+	var bid=$(obj).closest('tr').attr("id");
+	var cartlength = $("#cartLength").val();
+	cartlength--;
+	$("#cartLength").val(cartlength);
+    $(".badge").html(cartlength);
+	if(	!cartlength)  $("#Checkout").attr("disabled",true);
+	var bids = $("#brickids").val();
+	var bidsArray = bids.split("-"); 
+	var index = bidsArray.indexOf(realid);
+	if (index > -1) {
+   		 bidsArray.splice(index, 1);
+		 var bidsstring =bidsArray.join("-");
+		 $("#brickids").val(bidsstring);
+		 
+	}
+}
+
+function AddtoCampaignList(bid,avail,tc){
+
+	if($("#CampaignBody tbody tr").find("#" + bid).length>0	){
+		$("#addAlert").html("Brick : "+bid+" is already in your Campaign List");
+			$("#addAlert").fadeIn(3);
+			$("#addAlert").fadeOut(6000);
 	} else {
-		$('#error').html('Please enter a price');	
+
+		$("#Checkout").attr("disabled",false);
+		$("li #addAlert").html("Brick : "+bid+" was added to your Campaign List");
+		$("#addAlert").fadeIn(3);
+		$("#addAlert").fadeOut(6000);
+		var cartlength = $("#cartLength").val();
+		cartlength++;
+		$("#cartLength").val(cartlength);
+		$(".badge").html(cartlength);
+		data="<tr id='"+bid+"'><td><button onClick=\"RemoveBrick(this)\"  type=\"button\" class=\"btn btn-danger  btn-sm\" id=\""+bid+"\" title=\"Remove\">Remove</i></button></td><td>"+bid+"</td><td>" +tc+"</td><td>TBA</td><td>"+avail+ "</td></tr>";
+		$("#campaignModal .modal-body #CampaignBody tbody").append(data);
+		if (!$("#brickids").val()){
+			 $("#brickids").val(bid);
+		} else {
+			 $("#brickids").val($("#brickids").val() + "-" + bid);
+
+		}
 	}
 }
 function initialize() {
@@ -32,9 +67,18 @@ function initialize() {
 		);
 		var sc='#000000';
 		if(data[i].cost<=0.5){
-			var sc='#FFFF00'		
+			var sc='#fad201'	
+			var status='Under Construction';
+			var icon="yellow";	
+			var avail="TBA";
+			var no=0;
 		}else if(data[i].cost>0.5){
-			var sc='#00CC00'
+			var sc='#27e833';
+			var status='Avialable';
+			var icon="green";
+			var avail="Immediate";
+			var no=1;
+
 		}
 			
 		rectangle = new google.maps.Rectangle({
@@ -49,16 +93,29 @@ function initialize() {
 		});
 		rectangle.setMap(map);
 		infoWindow = new google.maps.InfoWindow();
-		content="<div class='container' style='width:300px;height:400px;overflow:hidden'>";
-		content= content+"<div class='row' style='width:300px;height:230;overflow:hidden'>";
-		content= content+"<span style='font-size:12px;'>Label:&nbsp;<span style='color:red;'>&nbsp;</span>&nbsp;&nbsp;<br>";
-		content= content+"<span style='font-size:12px;'>Brick Code:&nbsp;<span style='color:red;'>"+data[i].code2+"</span>&nbsp;&nbsp;<br>";
-		content= content+"<span style='font-size:12px;'>28 day:&nbsp;<span style='color:red;'>"+data[i].hit_count_last_period+"</span>&nbsp;&nbsp;<br>";
-		content= content+"<span style='font-size:12px;'>84 day:&nbsp;<span style='color:red;'>"+data[i].hit_count_last_three_periods+"</span>&nbsp;&nbsp;<br><br>";
-		content= content+"<span style='font-size:12px;'>Price:&nbsp;&nbsp;<span style='color:red;'><textarea id='"+data[i].code2+"' rows='10'  name='cost'>"+data[i].cost+"</textarea></span>&nbsp;&nbsp;<br><br>";
-		content= content+"<button type='button' class='btn btn-default save' id='savebutton' code='"+data[i].id+"' onClick=\"savePrice('"+data[i].code2+"','"+data[i].id+"')\">Save</button>";
-		content= content+"&nbsp;&nbsp;<span style='font-size:12px;' style='color:red;' id='error'></span></div></div>";
+		// content= content+"<div class='row' style='width:300px;height:230;overflow:hidden'>";
+		// content= content+"<span style='font-size:12px;'><span style='color:"+sc+";'>&nbsp;</span>"+status+"<br>";
+		// content= content+"<span style='font-size:12px;'>Brick Code:&nbsp;<span style='color:red;'>"+data[i].code2+"</span>&nbsp;&nbsp;<br>";
+		// content= content+"<span style='font-size:12px;'>28 day:&nbsp;<span style='color:red;'>"+data[i].hit_count_last_period+"</span>&nbsp;&nbsp;<br>";
+		// content= content+"<span style='font-size:12px;'>84 day:&nbsp;<span style='color:red;'>"+data[i].hit_count_last_three_periods+"</span>&nbsp;&nbsp;<br><br>";
+		// content= content+"<button type='button' class='btn btn-default save' id='savebutton' code='"+data[i].id+"' onClick=\"savePrice('"+data[i].code2+"','"+data[i].id+"')\">Save</button>";
+		// content= content+"&nbsp;&nbsp;<span style='font-size:12px;' style='color:red;' id='error'></span></div></div>";
+		
+
+		/*if(!j[i].statusCheck) buttons="<button type='button' class='btn btn-default'>Request Now</button>&nbsp;<button type='button' class='btn btn-default'>Add to Request List</button>";
+						else buttons="<button type='button' class='btn btn-default'>Add to Request List</button>";*/
+
+		if(no) buttons="<button type='button' class='btn btn-default' onClick='AddtoCampaignList("+data[i].code2+",\""+avail+"\","+data[i].hit_count_last_period+")' id='AddCampaign'>Add to Campaign</button>&nbsp;<button type='button' class='btn btn-default' data-toggle='modal' class='closeInfoWindow' data-bid='"+data[i].code2+"' data-target='#checkoutModel'>Request Now</button>";
+		else buttons="";
+		content="<div class='container' style='width:300px;height:225px;overflow:hidden'><div class='row' style='width:300px;height:275;overflow:hidden'><h3><img  src='i/"+icon+".png'>&nbsp;"+status+"</h3><span style='font-size:12px;'>Brick ID:&nbsp;<span style='color:red;'>"+data[i].code2+"</span>&nbsp;&nbsp;<i>"+status+"</i><br><br>28 day traffic counts: <span style='color:red;'>"+data[i].hit_count_last_period+"</span><br>Guaranteed Traffic Price Point: <span style='color:red;'>TBA</span>&nbsp;<br>Avialable:<span style='color:red;'>"+avail+"</span></span><br><br>"+buttons+"</div></div>";
 		createClickablePoly(rectangle, content, map);
+		var mapLabel = new MapLabel({
+          text: data[i].cost,
+          position: rectangle.bounds.getCenter(),
+          map: map,
+          fontSize: 10,
+          align: 'center'
+        });
 	}
 
   });
@@ -76,9 +133,69 @@ function createClickablePoly(poly, html, map) {
 		infoWindow.setContent(contentString);
 		infoWindow.setPosition(event.latLng);
 		infoWindow.open(map);
+
+
+
+
+
   });
 }
 $(document).ready(function() {
   initialize();
+  $("#addAlert").hide();
+  $("#campaignModal .modal-body #CampaignBody tbody").html("");
+  $("#cartLength").val("");
+
+  $(".closeInfoWindow").click(function(){
+  		$("#brickids").val($(".closeInfoWindow").data("bid"));
+  })
+
+$('#checkoutModel').on('show.bs.modal', function (event) {
+
+
+		console.log($("#brickids").val());
+		
+  		  	if (typeof infoWindow != "undefined") {
+			if (infoWindow) {
+				infoWindow.close();
+			}
+		}
+
+		$('#myTab a[href="#home"]').tab('show')
+
+  })
+
+
+$('.next-btn').click(function(){
+		$('.nav-tabs > .active').next('li').find('a').trigger('click');
+});
+
+$(".exec-btn").click(function (e) {
+
+
+
+
+			 $("#cartLength").val("");
+
+			 $(".badge").html("0");
+
+
+
+			$("#brickids").val("");
+			$('#myTab a[href="#home"]').tab('show')
+
+			$("#confirmModal").modal('show');
+
+
+
+});
+
+
+ $('#myTab a').click(function (e) {
+  e.preventDefault()
+  $(this).tab('show')
+})
+
+ $('#myTab a[href="#home"]').tab('show')
 
 });
