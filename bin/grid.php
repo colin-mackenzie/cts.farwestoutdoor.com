ecbad495ea919f7c7b9c5e35648f226d0874b073
Colin Mackenzie <cmackenzie@localhost.localdomain>
2015-07-08 12:06:37 -0600

    realtime data and ad central

--- bin/grid.php
+++ bin/grid.php
@@ -1,3 +1,33 @@
 <?
-echo file_get_contents("https://adcentral-staging.ivrnet.com/api/v1/bricks?channel_id=80159af0-0ebd-41f9-9a49-bf86337b26d5");
+include "db.php";
+$adcentral = file_get_contents("https://adcentral-staging.ivrnet.com/api/v1/bricks?channel_id=80159af0-0ebd-41f9-9a49-bf86337b26d5");
+
+	
+$adcentral_array=json_decode($adcentral,true);
+
+
+
+$grid_array=array();
+
+foreach($adcentral_array as $key => $value) {
+	$sql="select cost from grid WHERE id='".$value['id']."'";
+
+
+	$result=mysql_query($sql) or die(mysql_error());
+
+	$row=mysql_fetch_assoc($result)  or die(mysql_error());
+
+	
+	  
+	$value['cost']=$row['cost'];
+	$grid_array[$key]=$value;
+
+
+
+}
+
+echo json_encode($grid_array);
+
+
+
 ?>
